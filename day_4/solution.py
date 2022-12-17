fd5ca81e13409a864250d6115350008c1ce8ad6c
James Baird <j.baird94@gmail.com>
2022-12-17 17:29:06 +0000

    Day 4 part 1

---
+++ day_4/solution.py
@@ -0,0 +1,22 @@
+import os
+
+# Expand the section representation to a set of all sections
+# E.g. 1-4 becomes {1, 2, 3, 4}
+def expand_section(section):
+    start, end = section.split('-')
+    return set(range(int(start), int(end) + 1))
+
+input_file = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+subset_count = 0
+with open(input_file, 'r') as f:
+    for line in f:
+        # Get expanded sections for each elf
+        # Elves are separated by a comma
+        sections = [expand_section(section) for section in line.split(',')]
+        if sections[0].issubset(sections[1]):
+            subset_count += 1
+        elif sections[1].issubset(sections[0]):
+            subset_count += 1
+
+print(f'Number of subsets: {subset_count}')
