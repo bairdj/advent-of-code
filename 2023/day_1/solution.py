2af35c91768197d4482042cee81a78c6948a3b45
James Baird <j.baird94@gmail.com>
2023-12-24 15:41:34 +0000

    Refactor

--- 2023/day_1/solution.py
+++ 2023/day_1/solution.py
@@ -1,28 +1,21 @@
 import re
 
-def process_line(line: str) -> int:
+def process_line_part_1(line: str) -> int:
     """
     Extract the first and last digit in a line of text,
     and return them as a combined integer.
     """
-    pattern = re.compile(r"(\d).*(\d)")
-    result = pattern.search(line)
-    if result is None:
-        # If the line has exactly one digit, return it twice
-        single_digit_pattern = re.compile(r"\d")
-        all_matches = single_digit_pattern.findall(line)
-        if len(all_matches) == 1:
-            extracted_digit = all_matches[0]
-            return int(extracted_digit + extracted_digit)
-
-        raise ValueError("Line does not contain exactly one digit")
-    return int(result.group(1) + result.group(2))
+    pattern = re.compile(r"\d")
+    matches = pattern.findall(line)
+    if len(matches) == 0:
+        raise ValueError("No digits found in line.")
+    return int(matches[0] + matches[-1])
 
 
 def process_file(path: str) -> int:
     with open(path, "r") as f:
-        lines = [process_line(line) for line in f.readlines()]
+        lines = [process_line_part_1(line) for line in f.readlines()]
     return sum(lines)
 
 if __name__ == "__main__":
-    print(process_file("input.txt"))
+    print(f"Part 1 answer: {process_file('input.txt')}")
