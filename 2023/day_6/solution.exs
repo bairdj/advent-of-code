c3a1bae15a31a58dc234c2109c059d41b6c22e38
James Baird <j.baird94@gmail.com>
2024-12-01 23:38:19 +0000

    solve part 2

--- 2023/day_6/solution.exs
+++ 2023/day_6/solution.exs
@@ -23,6 +23,27 @@ defmodule AdventOfCode.Day6.Race do
     |> find_winning_charges()
     |> Enum.count()
   end
+
+  @doc """
+  d = distance, t = duration, c = charge
+
+  d = c * (t - c)
+
+  Rearrange as quadratic equation:
+  c^2 - t * c + d = 0
+
+  Solve for c using quadratic formula: ax^2 + bx + c = 0
+  """
+  def solve_for_charge(%__MODULE__{duration: duration, record: record}) do
+    a = 1
+    b = -duration
+    c = record
+
+    x1 = (-b + :math.sqrt(:math.pow(b, 2) - 4 * a * c)) / (2 * a)
+    x2 = (-b - :math.sqrt(:math.pow(b, 2) - 4 * a * c)) / (2 * a)
+
+    {x1, x2}
+  end
 end
 
 defmodule AdventOfCode.Day6.Solution do
@@ -44,6 +65,17 @@ defmodule AdventOfCode.Day6.Solution do
     |> Enum.map(&String.to_integer/1)
   end
 
+  def parse_input_part_2(path) do
+    File.read!(path)
+    |> String.split("\n")
+    # Select all digits in the line and condense them
+    |> Enum.map(fn line ->
+      Regex.scan(~r/\d+/, line)
+      |> Enum.join()
+      |> String.to_integer()
+    end)
+  end
+
   def run(path) do
     races =
       parse_input(path)
@@ -58,6 +90,19 @@ defmodule AdventOfCode.Day6.Solution do
       |> Enum.reduce(&Kernel.*/2)
 
     IO.puts("Part 1: #{part_1_answer}")
+
+    [race_2_time, race_2_distance] =
+      parse_input_part_2(path)
+
+    [min_solved, max_solved] =
+      %Race{duration: race_2_time, record: race_2_distance}
+      |> Race.solve_for_charge()
+      |> Tuple.to_list()
+      |> Enum.sort()
+
+    part_2_solutions = :math.floor(max_solved) - :math.ceil(min_solved) + 1
+
+    IO.puts("Part 2: #{part_2_solutions}")
   end
 end
 
