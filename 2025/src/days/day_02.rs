f84783e3d0b19508a02b1f62996ad03b35702b5c
James Baird <6016948+bairdj@users.noreply.github.com>
2025-12-02 21:38:42 +0000

    day 2 part 2

--- 2025/src/days/day_02.rs
+++ 2025/src/days/day_02.rs
@@ -2,30 +2,37 @@ use std::io::Read;
 use std::ops::Range;
 use std::fs::File;
 
-fn is_valid_number(number: usize) -> bool {
+fn is_valid_number(number: usize, max_repeats: Option<usize>) -> bool {
     let num_str = number.to_string();
+    let num_length = num_str.len();
 
-    if num_str.len() % 2 != 0 {
-        return true;
-    }
+    // Length of a repeated pattern must be a factor of the total length
+    let possible_lengths = (1..num_length).filter(|l| num_length % l == 0);
 
-    let midpoint = num_str.len() / 2;
-    let (segment_1, segment_2) = num_str.split_at(midpoint);
+    for poss_length in possible_lengths {
+        let buffer = &num_str[0..poss_length];
+        let repeats = num_length / poss_length;
 
-    segment_1.ne(segment_2)
-}
+        if max_repeats.is_some_and(|max| repeats > max) {
+            continue;
+        }
 
-fn sum_invalid_numbers(ranges: Vec<Range<usize>>) -> usize {
-    let mut sum = 0;
-    for range in ranges {
-        for n in range {
-            if !is_valid_number(n) {
-                sum += n;
-            }
+        let repeated_string = buffer.repeat(repeats);
+
+        if repeated_string.eq(&num_str) {
+            return false;
         }
     }
-    
-    sum
+
+    true
+}
+
+fn sum_invalid_numbers(ranges: &[Range<usize>], max_repeats: Option<usize>) -> usize {
+    ranges
+    .iter()
+    .flat_map(|range| range.clone())
+    .filter(|&num| !is_valid_number(num, max_repeats))
+    .sum()
 }
 
 fn extract_ranges(input: &str) -> Vec<Range<usize>> {
@@ -45,8 +52,11 @@ pub fn run(input: &mut File) {
 
     let ranges = extract_ranges(&buffer);
 
-    let result_part_1 = sum_invalid_numbers(ranges);
+    let result_part_1 = sum_invalid_numbers(&ranges, Some(2));
     println!("Part 1: {}", result_part_1);
+
+    let result_part_2 = sum_invalid_numbers(&ranges, None);
+    println!("Part 2: {}", result_part_2);
 }
 
 #[cfg(test)]
@@ -55,28 +65,14 @@ mod tests {
 
     #[test]
     fn test_is_valid_number() {
-        assert_eq!(is_valid_number(11), false);
-        assert_eq!(is_valid_number(12), true);
-        assert_eq!(is_valid_number(22), false);
-        assert_eq!(is_valid_number(101), true);
-        assert_eq!(is_valid_number(1698522), true);
-        assert_eq!(is_valid_number(38593859), false);
-    }
-
-    #[test]
-    fn test_extract_ranges() {
-        let input = "10-20,30-40,50-60";
-        let ranges = extract_ranges(input);
-        assert_eq!(ranges.len(), 3);
-        assert_eq!(ranges[0], 10..20);
-        assert_eq!(ranges[1], 30..40);
-        assert_eq!(ranges[2], 50..60);
-    }
-
-    #[test]
-    fn test_integration() {
-        let ranges = extract_ranges("11-22");
-        let result = sum_invalid_numbers(ranges);
-        assert_eq!(result, 33);
+        assert_eq!(is_valid_number(11, Some(2)), false);
+        assert_eq!(is_valid_number(12, Some(2)), true);
+        assert_eq!(is_valid_number(22, Some(2)), false);
+        assert_eq!(is_valid_number(101, Some(2)), true);
+        assert_eq!(is_valid_number(1698522, Some(2)), true);
+        assert_eq!(is_valid_number(38593859, Some(2)), false);
+        assert_eq!(is_valid_number(757575, None), false);
+        assert_eq!(is_valid_number(123412341234, None), false);
+        assert_eq!(is_valid_number(123412345678, None), true);
     }
 }
